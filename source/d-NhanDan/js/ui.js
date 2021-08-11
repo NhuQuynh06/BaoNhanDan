e0cf2429b56d5f179fe1e5ed252036c22a44f3bf
CPU60417\quynhttn3 <quynhtran0618@gmail.com>
2021-08-11 23:54:41 +0700

    fix bug

--- source/d-NhanDan/js/ui.js
+++ source/d-NhanDan/js/ui.js
@@ -1,79 +1,98 @@
-
-$('.chevron-wrap').click(function (e) {
-    const $this = $(e.target);
-    var id = $this.attr('data-link');
-    $('html, body').animate({ scrollTop: $('#' + id).offset().top }, 1000);
-});
-
-$('.dots').on('click', function () {
-    const prefix = 'chapter';
-    var id = Number($(this).attr('data-link')) - 1;
-    if (id <= 0) {
-        $('html, body').animate({ scrollTop: $('.box-style-1').offset().top }, 1000);
-    } else {
-        $('html, body').animate({ scrollTop: $('#' + prefix + id).offset().top }, 1000);
-    }
-
-});
-
 // get height container 3-article-text
 var heightWindown = window.innerHeight - 270;
 $(".section-article-text .box-style-10 .main-content").height(heightWindown);
 
 
 // make a button to scroll horizontally in div
+var scrollWidth = $(".main-content")[0].scrollWidth;
+var countSlide = (scrollWidth - scrollWidth % 1173) / 1173;
+console.log('countSlide', countSlide);
+
+window.onload = function () {
+    if (countSlide == 0) {
+        $(".main-content .rank-2").css("display", "block");
+    }
+};
 
 
+
+let index = 0;
 $('.slideNext').on('click', function (e) {
-    $('.main-content').animate({ scrollLeft: '+=1160' }, 1000);
+    $('.main-content').animate({ scrollLeft: '+=1173' }, 1000);
+    if (index < countSlide) {
+        index = index + 1;
+    }
+    // show rank-2
+    console.log('index', index);
+    if (index == countSlide) {
+        console.log('scroll left max');
+        setTimeout(() => {
+            $(".main-content .rank-2").css("display", "block");
+        }, 1100);
+    }
 });
 
 $('.slidePrev').on('click', function (e) {
-    $('.main-content').animate({ scrollLeft: '-=1160' }, 1000);
+    $('.main-content').animate({ scrollLeft: '-=1173' }, 1000);
+    if (index > 0) {
+        index = index - 1;
+    }
+    if (index == countSlide - 2 ) {
+        console.log('hide rank-2');
+        $(".main-content .rank-2").css("display", "none");
+    }
 });
+
 
 // auto scroll
 if (document.querySelector('.section-homepage')) {
+    var activeChapter = 1;
+    var lastScroll = 0;
+    var monitor = true;
+    $('.dots, .chevron-wrap').on('click', function (e) {
+        e.preventDefault();
+        monitor = false;
+        $('html, body').animate({ scrollTop: $('#chapter' + $(this).attr('data-link')).offset().top }, 1000, function () { monitor = true; });
+    });
     $(window).scroll(function () {
-        var windscroll = $(window).scrollTop();
-        // if (isInViewport(document.querySelector('.chevron1'))) {
-        //     element = document.querySelector('.chevron1');
-        //     domRect = element.getBoundingClientRect();
-        //     var spaceBelow = window.innerHeight - domRect.bottom;
-        //     if (spaceBelow >= 350) {
-        //         $('html, body').animate({ scrollTop: $('.chapter1').offset().top }, 1000);
-        //         element = document.querySelector('.chevron2');
-        //         domRect = element.getBoundingClientRect();
-        //     }
-        // }
-    
-        if (windscroll >= 100) {
-            $('.chapter').each(function (i) {
-                // The number at the end of the next line is how pany pixels you from the top you want it to activate.
-                if ($(this).position().top <= windscroll - 0) {
-                    $('.wrap-dots .dots.active').removeClass('active');
-                    $('.wrap-dots .dots').eq(i).addClass('active');
+        var scroll = $(window).scrollTop();
+        var height = $(window).height();
+
+        var chapter = 1;
+        $('.chapter').each(function (i) {
+            if ($(this).position().top <= scroll + height * 0.5) {
+                chapter = parseInt($(this).attr('id').replace('chapter', ''));
+            }
+        });
+        $('.wrap-dots .dots.active').removeClass('active');
+        $('.wrap-dots .dots').eq(chapter - 1).addClass('active');
+        activeChapter = chapter;
+
+        if (!monitor) { return; }
+
+        var monitorObject;
+        if (scroll > lastScroll) {
+            //down
+            monitorObject = $('#chapterbtn' + activeChapter);
+            if (monitorObject.length > 0) {
+                if (monitorObject.offset().top > scroll && monitorObject.offset().top < scroll + height * 0.6) {
+                    monitorObject.click();
                 }
-            });
+            }
         } else {
-            $('.wrap-dots .dots.active').removeClass('active');
-            $('.wrap-dots .dots:first').addClass('active');
+            //top
+            if (activeChapter > 1) {
+                monitorObject = $('#chapter' + activeChapter);
+                if (monitorObject.length > 0) {
+                    if (monitorObject.offset().top < scroll + height && monitorObject.offset().top > scroll + height * 0.6) {
+                        monitor = false;
+                        $('html, body').animate({ scrollTop: $('#chapterbtn' + (activeChapter - 1)).offset().top - height * 0.7 }, 1000, function () { monitor = true; });
+                    }
+                }
+            }
+
         }
+        lastScroll = scroll;
+
     }).scroll();
-    
-    // function isInViewport(el) {
-    //     const rect = el.getBoundingClientRect();
-    //     return (
-    //         rect.top >= 0 &&
-    //         rect.left >= 0 &&
-    //         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    //         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    
-    //     );
-    // }
 }
-
-//  dot
-
-
-
--- source/m-NhanDan/js/ui.js
+++ source/m-NhanDan/js/ui.js
@@ -1,17 +1,4 @@
 
-$('.chevron-wrap').click(function (e) {
-    const $this = $(e.target);
-    var id = $this.attr('data-link');
-    $('html, body').animate({ scrollTop: $('#' + id).offset().top }, 1000);
-});
-
-// > show / hide elements
-$("button.bars").click(function (e) {
-    e.preventDefault();
-    $(this).toggleClass("is-active");
-    $('.mobile-menu').toggleClass("show");
-});
-
 // hamburger-menu
 $('.hamburger').on('click', function () {
     $('.hamburger').toggleClass('animation');
@@ -26,4 +13,6 @@ $('.menu-toggle a').click(function (event) {
     $('.menu-toggle a').siblings('.sub-menu').not('.active').hide();
     $(this).siblings(".sub-menu").slideToggle("slow");
 })
+
+
 
